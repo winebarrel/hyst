a03538c4f7175a6bd1e81c7c2a273e1c6e666ca0
winebarrel <sgwr_dts@yahoo.co.jp>
2016-06-16 01:14:40 +0900

    Fix formula sha256

--- homebrew/hyst.rb
+++ homebrew/hyst.rb
@@ -5,7 +5,7 @@ class Hyst < Formula
 
   homepage 'https://github.com/winebarrel/hyst'
   url "https://github.com/winebarrel/hyst/releases/download/v#{VERSION}/hyst-v#{VERSION}-darwin-amd64.gz"
-  sha256 '...'
+  sha256 'ba99b5ec8016eae462671b51b2c754f38d7194db8d66b1a58ee6b375000c6840'
   version VERSION
   head 'https://github.com/winebarrel/hyst.git', :branch => 'master'
 
